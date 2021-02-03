90ca3e7ac7d66404b983fbe58f226f9079a39034
galinaenimaneva <galinaenimaneva@gmail.com>
2021-02-03 10:26:02 +0200

    area + circumference

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
-//The formula for the area of a rectangle S=a*b
+
+//The formula for the area of a rectangle is S = a*b
+//The formula for the rectangle circumference is P = 2*a + 2*b
 public class Main {
 
-     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.print("a = ");
-            int a = scanner.nextInt();
-         System.out.print("b = ");
-         int b = scanner.nextInt();
-            int area = a * b;
+        System.out.print("a = ");
+        int a = scanner.nextInt();
+        System.out.print("b = ");
+        int b = scanner.nextInt();
+        int area = a * b;
+        int circumference = 2*a + 2*b;
 
-            System.out.print("Rectangle area = ");
-            System.out.println(area);
-        }
+        System.out.print("Rectangle area = ");
+        System.out.println(area);
+        System.out.print("Rectangle circumference = ");
+        System.out.println(circumference);
     }
+}
