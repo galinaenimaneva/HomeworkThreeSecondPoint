3ad8cfabaf500260e65fad3e2ca7537356339ffe
galinaenimaneva <galinaenimaneva@gmail.com>
2021-02-03 11:24:19 +0200

    area + circumference + positive or negative number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.print("b = ");
         int b = scanner.nextInt();
         int area = a * b;
-        int circumference = 2*a + 2*b;
+        int circumference = 2 * a + 2 * b;
 
         System.out.print("Rectangle area = ");
         System.out.println(area);
@@ -20,3 +20,19 @@ public class Main {
         System.out.println(circumference);
     }
 }
+
+class PositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        if (number == 0) {
+            System.out.println("Number is equal to zero!");
+        } else if (number > 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is negative");
+        }
+    }
+}
+
